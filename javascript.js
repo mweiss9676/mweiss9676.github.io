612bbd465555c2f53218643a99f845051f424e4a
Michael Weiss <mweis9676@gmail.com>
2018-02-27 21:05:09 -0500

    a lot of work, navbars installed and some text added

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("container").style.marginLeft = "250px";
+    document.getElementById("michael").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("container").style.marginLeft = "0";
+    document.getElementById("michael").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
